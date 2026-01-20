4718b9cb7c0f0b7cf24367a9da70f7ae42a03919
bindupatel-19 <bindugombe199@gmail.com>
2026-01-20 22:09:11 +0530

    add responsive benefits section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 "use client";
 
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Benefits from "./components/Benefits";
 import Button from "./components/Button";
+import Collaboration from "./components/Collaboration";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Section from "./components/Section";
@@ -11,7 +13,8 @@ const App = () => {
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
-        <Section />
+        <Benefits />
+        <Collaboration />
       </div>
 
       <ButtonGradient />
