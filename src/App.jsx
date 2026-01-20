c94ad6d1865dae0c54887d49e20babe21d60053c
bindupatel-19 <bindugombe199@gmail.com>
2026-01-20 19:41:12 +0530

    add responsive hero section with parallax effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+"use client";
+
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Button from "./components/Button";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Section from "./components/Section";
 const App = () => {
   return (
     <>
-      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden"></div>
-      <Header />
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+        <Hero />
+        <Section />
+      </div>
+
       <ButtonGradient />
     </>
   );
